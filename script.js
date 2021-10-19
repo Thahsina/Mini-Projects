52c7c84581694ccfd6a1a7ef241da4f56f8099ee
Thahsina <s.thahsina@gmail.com>
2021-10-19 20:13:14 +0300

    Mini Project 1

---
+++ script.js
@@ -0,0 +1,15 @@
+// setInterval(function() {
+// const now = new Date();
+// const hours = now.getHours();
+// const minutes = now.getMinutes();
+// const seconds = now.getSeconds();
+
+// const clock = `Clock ${hours}:${minutes}:${seconds}`;
+// // console.log(clock);
+
+// }, 1000);
+
+
+const message = document.createElement('div');
+
+message.classList.add("cookie-message");
