7ee9cf905b2ee858b000b28976e8659d6d555222
Thahsina <50306556+Thahsina@users.noreply.github.com>
2021-12-13 23:23:33 +0300

    Add files via upload

---
+++ Mobile Nav/script.js
@@ -0,0 +1,24 @@
+const buttons = document.querySelectorAll('ul li button')
+const contents = document.querySelectorAll('.content')
+
+buttons.forEach((item,idx) => {
+    item.addEventListener('click', ()=>{
+        hideAllContents()
+    hideAllItems()
+
+    item.classList.add('active')
+    contents[idx].classList.add('show')
+
+    })
+
+    
+})
+
+function hideAllContents() {
+    contents.forEach(content => content.classList.remove('show'))
+}
+
+
+function hideAllItems() {
+    buttons.forEach(item => item.classList.remove('active'))
+}
